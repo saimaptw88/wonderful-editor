8ee3ac2f8405fa1e42238fbdbbef1ace753ea257
斎藤正大 <saima.ptw88@gmail.com>
2021-02-21 15:33:47 +0900

    retouch

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resourses :article
+      resources :article
     end
   end
   mount_devise_token_auth_for "User", at: "auth"
